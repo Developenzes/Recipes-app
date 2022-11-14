239196e2dbf281c0b32414fac6bcf563bee4af81
Developenzes <miroslav.penzes@gmail.com>
2022-11-14 10:42:55 +0100

    It is possible to change serving count

--- src/components/RecipeDetailIngredients.js
+++ src/components/RecipeDetailIngredients.js
@@ -1,18 +1,26 @@
 import { Table, Input } from 'reactstrap';
 import './RecipeDetailIngredients.css';
+import { useState } from 'react';
 
 export default function RecipeDetailIngredients({ ingredients, servingCount }) {
+
+  const [newServingCount, setNewServingCount] = useState(servingCount)
+
   return (
     <div>
       {servingCount && (
         <div className="serving--count">
           <span>Počet porcií:</span>
-          <Input type="number" min="1" defaultValue={servingCount} />
+          <Input type="number" min="1" defaultValue={servingCount} onChange={(e) => setNewServingCount(e.target.value)} />
         </div>
       )}
       <Table striped>
         <tbody>
-          {ingredients?.map((ingredient) =>
+          {ingredients?.map((ingredient) => {
+
+          const oneServingCount = ingredient.amount / servingCount
+
+          return (
             ingredient.isGroup ? (
               <tr key={ingredient._id} className="text-center">
                 <td colSpan={3} className="table-primary">
@@ -21,11 +29,11 @@ export default function RecipeDetailIngredients({ ingredients, servingCount }) {
               </tr>
             ) : (
               <tr key={ingredient._id}>
-                <td className="text-alighn-right">{ingredient.amount}</td>
+                <td className="text-alighn-right">{!ingredient.amount ? "" : (oneServingCount * newServingCount)}</td>
                 <td>{ingredient.amountUnit}</td>
                 <td>{ingredient.name}</td>
               </tr>
-            ),
+            ))}
           )}
         </tbody>
       </Table>
